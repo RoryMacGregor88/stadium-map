8d4064d914e7f84a013e0984ec871a14eed8bc2f
Rory MacGregor <rory.macgregor@astrosat.space>
2019-10-17 15:59:53 +0100

    Properly set up.

--- src/map/Map.js
+++ src/map/Map.js
@@ -6,26 +6,32 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoicm9yeW1hY2dyZWdvcjg4IiwiYSI6ImNrMWozNndycDA3N
 
 export default class Map extends React.Component {
 
-    map;
-
     constructor(props) {
         super(props);
         this.state = {
-
+            map: null
         }
+        this.mapInit = this.mapInit.bind(this);
     }
 
-    map = new mapboxgl.Map({
-        container: this.container,
-        style: 'mapbox://styles/rorymacgregor88/ck1uqgvah0afo1clni9co56yd',
-        center: [1.1743, 52.3555],
-        zoom: 7.5
-      })
+    componentDidMount() {
+        this.mapInit();
+    }
+
+    mapInit() {
+        const map = new mapboxgl.Map({
+            container: this.mapContainer,
+            style: 'mapbox://styles/rorymacgregor88/ck1uqgvah0afo1clni9co56yd',
+            center: [1.1743, 52.3555],
+            zoom: 7.5
+        })
+        this.setState({map})
+    }
 
     render() {
         return (
             <div 
-                ref={div => this.container = div}
+                ref={div => this.mapContainer = div}
                 className='map-container'
             >
             </div>
