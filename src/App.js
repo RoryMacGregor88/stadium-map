90db32fab716fbe960b10c848790bf0290884052
Rory MacGregor <rory.macgregor@astrosat.space>
2019-10-18 17:16:03 +0100

    Broke it.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Map from './map/Map';
+import SideBar from './sidebar/SideBar';
 
 function App() {
   return (
+    <div>
+      <SideBar />
       <Map />
+    </div>
   );
 }
 
--- src/map/Map.js
+++ src/map/Map.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './map.css';
 import mapboxgl from 'mapbox-gl';
-import Toggle from '../components/Toggle'
+import Toggle from '../toggle/Toggle'
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoicm9yeW1hY2dyZWdvcjg4IiwiYSI6ImNrMWozNndycDA3NTMzaXA3bDBvbHY0dXUifQ.FVuzhYeFcbrlDJTnAUXM3Q';
 
@@ -14,6 +14,7 @@ export default class Map extends React.Component {
             showStadiums: true
         }
         this.mapInit = this.mapInit.bind(this);
+        this.switchThemes = this.switchThemes.bind(this);
         this.toggleStadiums = this.toggleStadiums.bind(this);
     }
 
@@ -30,38 +31,70 @@ export default class Map extends React.Component {
         })
 
         map.on('load', () => {
-
             this.setState({map})
 
-            map.on('click', (evt) => {
-
-                const feature = map.queryRenderedFeatures(evt.point);
+            map.on('click', 'stadiums',  (evt) => {
+                const features = map.queryRenderedFeatures(evt.point);
+                console.log(features)
 
                 new mapboxgl.Popup({offset: 25})
                     .setLngLat(evt.lngLat)
-                    .setHTML('<h1>' + 'Team: ' + feature[0].properties.team + '</h1>')
+                    .setHTML('<h1>' + features[0].properties.team + '</h1>')
                     .addTo(map);
             })
         })
     }
 
-toggleStadiums() {
-    const map = this.state.map;
-    this.state.showStadiums
-    ? map.setLayoutProperty('stadiums', 'visibility', 'none') 
-    : map.setLayoutProperty('stadiums', 'visibility', 'visible');
+    toggleStadiums() {
+        const map = this.state.map;
 
-    this.setState({showStadiums: !this.state.showStadiums});
-}
+        this.state.showStadiums
+        ? map.setLayoutProperty('stadiums', 'visibility', 'none') 
+        : map.setLayoutProperty('stadiums', 'visibility', 'visible');
+
+        this.setState({showStadiums: !this.state.showStadiums});
+    }
+
+    switchThemes(evt) {
+        const map = this.state.map;
+
+        if (evt.target.name === 'dark') {
+            map.setStyle('mapbox://styles/rorymacgregor88/ck1w2muag09d11cs2dbosj01f');
+            map.on('load', () => {
+                map.addLayer({
+                    id: 'stadiums',
+                    source: {
+                      type: 'vector',
+                      url: 'rorymacgregor88.ck1uq4pa806kt2os8ygktg2ar-5aobc'
+                    }
+                })
+            })
+        } else {
+            map.setStyle('mapbox://styles/rorymacgregor88/ck1uqgvah0afo1clni9co56yd')
+        }
+    }
 
     render() {
  
         return (
             <div>
-                {/* I only made the toggle button into a component to see if it was possble. */}
                 <Toggle 
                     toggle={this.toggleStadiums}
                 />
+                <div className='menu'>
+                    <button
+                        name='light' 
+                        type='radio'
+                        onClick={this.switchThemes}
+                        >Light Mode
+                    </button>
+                    <button 
+                        name='dark'
+                        type='radio'
+                        onClick={this.switchThemes}
+                        >Dark Mode
+                    </button>
+                </div>
                 <div 
                     ref={div => this.mapContainer = div}
                     className='map-container'
